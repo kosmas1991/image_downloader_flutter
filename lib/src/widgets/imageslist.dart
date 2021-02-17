9a11288fd018765247879e654ec3d3f4959ad930
kosmas1991 <kosmasgou@gmail.com>
2021-02-17 12:02:02 +0200

    final

--- lib/src/widgets/imageslist.dart
+++ lib/src/widgets/imageslist.dart
@@ -3,15 +3,31 @@ import '../models/imagemodel.dart';
 
 class ImageList extends StatelessWidget {
   ImageList(this.images);
+
   final List<ImageModel> images;
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-     // return Image.network(images[index].url);
-      return Image.network(images[index].url);
-    },
-    itemCount: images.length,
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        // return Image.network(images[index].url);
+        return Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black26)),
+          padding: EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  child: Image.network(images[index].url),
+                  padding: EdgeInsets.only(
+                    bottom: 8.0,
+                  )),
+              Text('a text here')
+            ],
+          ),
+        );
+      },
+      itemCount: images.length,
     );
   }
-
 }
